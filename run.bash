d950c2412730719f8fa67c95f742eb35e96f6585
louiswng <louiswong.cs@connect.hku.hk>
2022-10-15 18:32:10 +0800

    update hyperparams

--- run.bash
+++ run.bash
@@ -1,10 +1,5 @@
 # !/bin/bash
 
-# python3 LightGCN.py --data ciao --save_name ciao_lightgcn --epoch 200 --lr 1e-3 --gnn_layer 2 --reg 1e-5 --gpu 0
-
-# python3 SGL.py --data ciao --save_name ciao_sgl --epoch 200 --lr 1e-3 --ssl_reg 1e-6 --gnn_layer 2 --keepRate 0.5 --temp 0.4 --reg 1e-5 --gpu 0
-
-# python3 SHT.py --data ciao --save_name ciao_sht --epoch 300 --lr 1e-4 --ssl_reg 1e-6 --edgeSampRate 0.1 --gnn_layer 3 --hgnn_layer 2 --hyperNum 128 --att_head 4 --leaky 0.5 --reg 1e-2 --gpu 0
-
-python3 our.py --data ciao --save_name ciao_our --epoch 300 --lr 1e-3 --mult 1e2 --uuPre_reg 1e-3 --sal_reg 1e-5 --ssl_reg 1e-6 --edgeSampRate 0.1 --gnn_layer 3 --hgnn_layer 2 --hyperNum 128 --att_head 4 --keepRate 0.5 --dropRate 0.5 --temp 1.0 --leaky 0.5 --reg 1e-2 --gpu 0
-
+# python3 our.py --data ciao --save_name ciao_our --epoch 150 --lr 1e-3 --batch 2048 --latdim 64 --gnn_layer 4 --sBatch 1024 --uuPre_reg 1e-1 --uugnn_layer 1 --sal_reg 1e-5 --ssl_reg 1e-8 --temp 1.0 --reg 1e-5 --gpu 0
+# python3 our.py --data epinions --save_name epinions_our --epoch 200 --lr 1e-3 --batch 2048 --latdim 64 --gnn_layer 4 --sBatch 2048 --uuPre_reg 1e-3 --uugnn_layer 4 --sal_reg 1e-5 --ssl_reg 1e-7 --temp 1.0 --reg 1e-5 --gpu 0
+python3 our.py --data yelp --save_name yelp_our --epoch 200 --lr 1e-3 --batch 2048 --latdim 64 --gnn_layer 4 --sBatch 2048 --uuPre_reg 1e-2 --uugnn_layer 4 --sal_reg 1e-6 --ssl_reg 1e-7 --temp 1.0 --reg 1e-5 --gpu 0
